7ea1a376485805ddb38e5dda8dad4efa198ca216
toshi <toshi.sys.co.jp@gmail.com>
2025-10-22 16:04:36 +0900

    コメント修正

--- src/Test6_1.php
+++ src/Test6_1.php
@@ -2,6 +2,11 @@
 
 /**
  * 等価演算子の解釈がPHP7からPHP8で変わった！
+ * 
+ * プログラム実行　↓
+ * ・「Ctrl+Shift+P」
+ * ・「Tasks: Run Task」選択
+ * ・実行するバージョンのタスク（Run With PHP X.X）を選択
  */
 
 echo "PHP Ver." . phpversion() . "\n\n";
